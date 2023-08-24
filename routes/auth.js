889a55e2973e580662a5fb6aa7bd4535dcfd6ac5
[SW5] 김경연 <argand0304@gmail.com>
2023-08-24 13:29:55 +0900

    feat:check-login,etc

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,12 @@
-import { Router }  from 'express';
-import passport from 'passport';
-import { login, join } from '../controllers/auth.js'
+import { Router } from "express";
+import passport from "passport";
+import { login, join, checkLogin } from "../controllers/auth.js";
 
 const router = Router();
 
-router.post(
-    '/api/login',
-    passport.authenticate('local', { session: false }),
-    login
-);
+router.post("/api/login", passport.authenticate("local", { session: false }), login);
 
-router.post('/api/join', join);
+router.post("/api/join", join);
+router.get("/api/check-login", checkLogin);
 
 export default router;
