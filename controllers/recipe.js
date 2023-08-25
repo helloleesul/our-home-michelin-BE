ba608c9c8084db25d26d06018862b8af71c7c315
[SW5] 양유진 <yoojinyang0303@gmail.com>
2023-08-26 04:08:35 +0900

    Feat: 에디터, 에디터의 레시피 목록 조회 추가, 그외 컨트롤러 추가 수정

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -2,205 +2,195 @@ import Recipe from "../models/recipe.js";
 import User from "../models/user.js";
 import Editor from "../models/editor.js";
 
-// (postman 체크 O) 전체 레시피 조회
-// 실행 시, .slice 하면 에러 발생 Cannot read properties of undefined (reading 'slice') -- 프론트 오피스아워때 질문 해보기로 (선정님)
+// 전체 레시피 조회
 export const getAllRecipes = async (req, res) => {
-  try {
-    const defaultLimit = await Recipe.countDocuments(); // defaultLimit - 전체 레시피 수
-    const limit = parseInt(req.query.limit) || defaultLimit;
-    let allRecipes;
+    try {
+        const defaultLimit = await Recipe.countDocuments(); // defaultLimit - 전체 레시피 수
+        const limit = parseInt(req.query.limit) || defaultLimit;
+        let allRecipes;
 
-    if (limit !== defaultLimit) {
-      if (limit > 0) {
-        allRecipes = await Recipe.find().sort({ createdDate: -1 }).limit(limit);
-      } else {
-        // limit 값이 음의 정수인 경우 -> defaultLimit
-        allRecipes = await Recipe.find()
-          .sort({ createdDate: -1 })
-          .limit(defaultLimit);
-      }
-    } else {
-      allRecipes = await Recipe.find()
-        .sort({ createdDate: -1 })
-        .limit(defaultLimit);
+        if (limit !== defaultLimit) {
+            if (limit > 0) {
+                allRecipes = await Recipe.find().sort({ createdDate: -1 }).limit(limit);
+            } else {
+                // limit 값이 음의 정수인 경우 -> defaultLimit
+                allRecipes = await Recipe.find().sort({ createdDate: -1 }).limit(defaultLimit);
+            }
+        } else {
+            allRecipes = await Recipe.find().sort({ createdDate: -1 }).limit(defaultLimit);
+        }
+        res.status(200).json(allRecipes);
+    } catch (err) {
+        res.status(500).json({ message: "문제가 발생했습니다." });
     }
-    res.status(200).json(allRecipes);
-  } catch (err) {
-    res.status(500).json({ message: "문제가 발생했습니다." });
-  }
 };
 
-// (postman 체크 O) 특정 레시피(recipeId) 조회
+// 특정 레시피(recipeId) 조회
 export const getRecipe = async (req, res) => {
-  try {
-    const recipeId = req.params.id;
-    const recipe = await Recipe.findById(recipeId);
-    if (!recipe) {
-      // 해당 레시피가 없는 경우
-      return res.status(404).json({ message: "레시피를 찾을 수 없습니다 :(" });
+    try {
+        const recipeId = req.params.id;
+        const recipe = await Recipe.findById(recipeId);
+        if (!recipe) {
+            // 해당 레시피가 없는 경우
+            return res.status(404).json({ message: "레시피를 찾을 수 없습니다 :(" });
+        }
+        res.status(200).json(recipe);
+    } catch (err) {
+        res.status(500).json({ message: "문제가 발생했습니다." });
     }
-    res.status(200).json(recipe);
-  } catch (err) {
-    res.status(500).json({ message: "문제가 발생했습니다." });
-  }
 };
 
-// (postman 체크 O) 5스타 레시피(=인기 레시피) 조회
+// 5스타 레시피(=인기 레시피) 조회
 export const getFiveStarRecipes = async (req, res) => {
-  try {
-    const defaultLimit = await Recipe.countDocuments({
-      likeCount: { $gte: 20 },
-    }); // defaultLimit - '좋아요' 20개 이상의 글의 '수'
-    const limit = parseInt(req.query.limit) || defaultLimit;
-    let fiveStarRecipes;
+    try {
+        const defaultLimit = await Recipe.countDocuments({
+            likeCount: { $gte: 20 },
+        }); // defaultLimit - '좋아요' 20개 이상의 글의 '수'
+        const limit = parseInt(req.query.limit) || defaultLimit;
+        let fiveStarRecipes;
 
-    if (limit !== defaultLimit) {
-      if (limit > 0) {
-        fiveStarRecipes = await Recipe.find({
-          likeCount: { $gte: 20 },
-        })
-          .sort({ likeCount: -1 })
-          .limit(limit);
-      } else {
-        // limit 값이 음의 정수인 경우 -> defaultLimit
-        fiveStarRecipes = await Recipe.find({
-          likeCount: { $gte: 20 },
-        })
-          .sort({ likeCount: -1 })
-          .limit(defaultLimit);
-      }
-    } else {
-      fiveStarRecipes = await Recipe.find({
-        likeCount: { $gte: 20 },
-      })
-        .sort({ likeCount: -1 })
-        .limit(defaultLimit);
+        if (limit !== defaultLimit) {
+            if (limit > 0) {
+                fiveStarRecipes = await Recipe.find({
+                    likeCount: { $gte: 20 },
+                })
+                    .sort({ likeCount: -1 })
+                    .limit(limit);
+            } else {
+                // limit 값이 음의 정수인 경우 -> defaultLimit
+                fiveStarRecipes = await Recipe.find({
+                    likeCount: { $gte: 20 },
+                })
+                    .sort({ likeCount: -1 })
+                    .limit(defaultLimit);
+            }
+        } else {
+            fiveStarRecipes = await Recipe.find({
+                likeCount: { $gte: 20 },
+            })
+                .sort({ likeCount: -1 })
+                .limit(defaultLimit);
+        }
+        res.status(200).json({ message: "5스타 레시피 목록 조회 성공", fiveStarRecipes });
+    } catch (err) {
+        res.status(500).json({ message: "문제가 발생했습니다." });
+        console.log(err);
     }
-    res
-      .status(200)
-      .json({ message: "5스타 레시피 목록 조회 성공", fiveStarRecipes });
-  } catch (err) {
-    res.status(500).json({ message: "문제가 발생했습니다." });
-    console.log(err);
-  }
 };
 
-// (아직 체크 X) 에디터 목록 & 에디터의 레시피 목록 조회
+// 에디터 목록 & 에디터의 레시피 목록 조회
 export const getEditorsRecipes = async (req, res) => {
-  try {
-    // 좋아요 100개 이상(= 에디터 자격) 받은 에디터 조회
-    const editors = await Editor.find({ rank2023: { $gte: 100 } });
+    try {
+        // 좋아요 100개 이상(= 에디터 자격) 받은 레시피가 있는 에디터 조회
+        const editors2022 = await Editor.find({
+            rank2022: { $gte: 1, $lte: 10 },
+        })
+            .sort({ rank2022: 1 })
+            .limit(10);
+        const editors2023 = await Editor.find({
+            rank2023: { $gte: 1, $lte: 10 },
+        })
+            .sort({ rank2022: 1 })
+            .limit(10);
 
-    const editorsWithRecipes = [];
+        const editorsWithRecipes = [];
 
-    // 각 에디터의 레시피 목록 조회
-    for (const editor of editors) {
-      const editorId = editor.userId;
-      const recipes = await Recipe.find({ editorId, likeCount: { $gte: 100 } });
-      editorsWithRecipes.push({ editor, recipes });
+        // 각 에디터의 레시피 목록 조회
+        for (const editor of [...editors2022, ...editors2023]) {
+            const editorId = editor.userId;
+            const recipes = await Recipe.find({ writerId: editorId });
+            editorsWithRecipes.push({ editor, recipes });
+        }
+
+        res.status(200).json({
+            message: "에디터, 에디터가 작성한 레시피 목록 조회 성공",
+            editorsWithRecipes,
+        });
+    } catch (err) {
+        res.status(500).json({ message: "문제가 발생했습니다." });
+        console.log(err);
     }
-
-    res.status(200).json({
-      message: "에디터, 에디터가 작성한 레시피 목록 조회 성공",
-      editorsWithRecipes,
-    });
-  } catch (err) {
-    res.status(500).json({ message: "문제가 발생했습니다." });
-  }
 };
 
-// (백 & 프론트 체크 O) 레시피 작성
+// 레시피 작성
 export const writeRecipe = async (req, res) => {
-  try {
-    const {
-      title,
-      recipeType,
-      recipeServing,
-      process,
-      ingredients,
-      imageUrl,
-      createdDate,
-    } = req.body;
+    try {
+        const { title, recipeType, recipeServing, process, ingredients, imageUrl, createdDate, writerId } = req.body;
 
-    const recipe = await Recipe.create({
-      title,
-      recipeType,
-      recipeServing,
-      process,
-      ingredients,
-      imageUrl,
-      likeCount: 0,
-      createdDate,
-    });
+        const recipe = await Recipe.create({
+            title,
+            recipeType,
+            recipeServing,
+            process,
+            ingredients,
+            imageUrl,
+            likeCount: 0,
+            createdDate,
+            writerId,
+        });
 
-    res.json(recipe);
-  } catch (err) {
-    res.status(500).send("레시피 등록 과정에서 오류가 발생되었습니다.");
-    console.log(err);
-  }
+        res.json(recipe);
+    } catch (err) {
+        res.status(500).send("레시피 등록 과정에서 오류가 발생되었습니다.");
+        console.log(err);
+    }
 };
 
-// (postman 체크 중) - 404 에러 왜?
+// 레시피 수정 - (404 에러 - 원인 찾는 중)
 export const updateRecipe = async (req, res) => {
-  try {
-    const recipeId = req.params._id; // 레시피 자체의 id
-    // 존재하는 레시피인지 확인
-    const existingRecipe = await Recipe.findById(recipeId);
+    try {
+        const { title, recipeType, recipeServing, process, ingredients, imageUrl, likeCount, writerId } = req.body;
+        const recipeId = req.params._id; // 레시피 자체의 id
+        // 존재하는 레시피인지 확인
+        const existingRecipe = await Recipe.findById(recipeId);
 
-    if (!existingRecipe) {
-      return res.status(404).json({ message: "레시피를 찾을 수 없습니다." });
+        if (!existingRecipe) {
+            return res.status(404).json({ message: "레시피를 찾을 수 없습니다." });
+        }
+
+        // 레시피 작성자 id인지 레시피 수정 권한 확인
+        if (existingRecipe.writerId.toString() !== req.user._id) {
+            return res.status(403).json({ message: "해당 레시피에 대한 수정 권한이 없습니다." });
+        }
+
+        const recipeUpdateData = {
+            title,
+            recipeType,
+            recipeServing,
+            process,
+            ingredients,
+            imageUrl,
+            likeCount,
+            writerId,
+        };
+
+        const updatedRecipe = await Recipe.findByIdAndUpdate(recipeId, recipeUpdateData, { new: true });
+
+        res.json(updatedRecipe);
+    } catch (err) {
+        res.status(500).send("레시피 수정 과정에서 오류가 발생되었습니다.");
     }
-
-    // 레시피 작성자 id인지 레시피 수정 권한 확인
-    if (existingRecipe.writerId.toString() !== req.user._id) {
-      return res
-        .status(403)
-        .json({ message: "해당 레시피에 대한 수정 권한이 없습니다." });
-    }
-    const {
-      title,
-      recipeType,
-      recipeServing,
-      process,
-      ingredients,
-      imageUrl,
-      likeCount,
-    } = req.body;
-
-    const recipeUpdateData = {
-      title,
-      recipeType,
-      recipeServing,
-      process,
-      ingredients,
-      imageUrl,
-      likeCount,
-    };
-
-    const updatedRecipe = await Recipe.findByIdAndUpdate(
-      recipeId,
-      recipeUpdateData,
-      { new: true }
-    );
-
-    res.json(updatedRecipe);
-  } catch (err) {
-    res.status(500).send("레시피 수정 과정에서 오류가 발생되었습니다.");
-  }
 };
 
-// (postman 체크 O) 특정 레시피(:id) 삭제
+// 레시피 삭제
 export const deleteRecipe = async (req, res) => {
-  // try, catch 공통적으로..
-  const recipeId = req.params.id;
+    const recipeId = req.params.id;
+    const userId = req.user._id; // 로그인한 회원id (자신이 작성한 레시피만 삭제 가능해야해서)
 
-  try {
-    const deletedRecipe = await Recipe.findByIdAndDelete(recipeId);
-    if (!deletedRecipe) {
-      return res.status(404).json({ message: "레시피를 찾을 수 없습니다 :(" });
+    try {
+        const recipeToDelete = await Recipe.findById(recipeId);
+
+        if (!recipeToDelete) {
+            return res.status(404).json({ message: "레시피를 찾을 수 없습니다 :(" });
+        }
+
+        if (!recipeToDelete.writerId.equals(userId)) {
+            return res.status(403).json({ message: "자신이 작성한 레시피만 삭제할 수 있습니다." });
+        }
+
+        const deletedRecipe = await Recipe.findByIdAndDelete(recipeId);
+        return res.json({ message: "레시피 삭제가 완료되었습니다." });
+    } catch (err) {
+        res.status(500).json({ message: "문제가 발생했습니다." });
     }
-    return res.json({ message: "레시피 삭제가 완료되었습니다." });
-  } catch (err) {
-    res.status(500).json({ message: "문제가 발생했습니다." });
-  }
 };
--- models/recipe.js
+++ models/recipe.js
@@ -4,20 +4,20 @@ import User from "./user.js";
 const Schema = mongoose.Schema;
 
 const ingredientObj = new Schema({
-  name: { type: String, required: true }, // name -> id (ingredient 컬렉션 참조 1-recipe 데이터를 가져온다. ingredient 컬렉션 향해서 참조 // 2-mongoose에 populate라는 기능이 있다. 해당 아이디가 참조하고 있는 값까지 합쳐서 데이터를 달라. 는 내용
-  amount: { type: String, required: true },
+    name: { type: String, required: true },
+    amount: { type: String, required: true },
 });
 
 const recipeSchema = new Schema({
-  title: { type: String, required: true },
-  recipeServing: { type: Number, required: true, default: 1 },
-  recipeType: { type: String, required: true },
-  process: { type: Array, required: true },
-  ingredients: { type: [ingredientObj], required: true }, // ingredient collection이 있어야할 것 같다.
-  imageUrl: { type: String, required: false }, // multer 적용 전까지는 post 테스트 등을 위해 'false'로 잠시 변경
-  likeCount: { type: Number, required: true },
-  writerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // ObjectId, userId ... // '레시피 작성자' 넣어야 함. type: String (?) -- owner id
-  createdDate: { type: Date, default: Date.now }, // '최신순' 정렬을 위하여 레시피 작성 날짜 필요
+    title: { type: String, required: true },
+    recipeServing: { type: Number, required: true, default: 1 },
+    recipeType: { type: String, required: true },
+    process: { type: Array, required: true },
+    ingredients: { type: [ingredientObj], required: true },
+    imageUrl: { type: String, required: false }, // 이미지 등록 안하고 저장하는 레시피는 '냉슐랭 레시피 기본 대표 이미지' 저장되도록 함.
+    likeCount: { type: Number, required: true },
+    writerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // ObjectId, userId ... // '레시피 작성자' 넣어야 함. type: String (?) -- owner id
+    createdDate: { type: Date, default: Date.now },
 });
 
 export default mongoose.model("Recipe", recipeSchema);
