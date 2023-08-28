f386ef493250e181ff5819a14bff36d66472ea0e
[SW5] 김경연 <argand0304@gmail.com>
2023-08-28 16:26:21 +0900

    fix:editor_sorted_pagination

--- controllers/editor.js
+++ controllers/editor.js
@@ -12,17 +12,29 @@ export const getEditorRecipes = async (req, res, next) => {
         .json({ message: "해당 ID의 에디터를 찾을 수 없습니다." });
     }
 
-    const recipes = await Recipe.find({ writer: editorId }).populate(
-      "writer",
-      "nickName"
-    );
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 20;
+
+    const skip = (page - 1) * limit;
+
+    const total = await Recipe.countDocuments({ writer: editorId });
+
+    const recipes = await Recipe.find({ writer: editorId })
+      .sort({ createdDate: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("writer", "nickName");
 
     if (!recipes.length) {
       return res
         .status(404)
         .json({ message: "해당 에디터의 레시피를 찾을 수 없습니다." });
     }
-    res.json(recipes);
+    res.json({
+      recipes,
+      totalPages: Math.ceil(total / limit),
+      currentPage: page,
+    });
   } catch (error) {
     next(error);
   }
