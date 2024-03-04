1aac47a71247b1b990aeff890f8fe6ff67e0e909
LEE SUL <suerish.e@gmail.com>
2024-03-04 15:57:42 +0900

    refactor: JWT payload 수정

--- utils/token.js
+++ utils/token.js
@@ -3,11 +3,7 @@ import jwt from "jsonwebtoken";
 const generateToken = (user) => {
   const payload = {
     _id: user._id,
-    email: user.email,
     nickName: user.nickName,
-    password: user.password,
-    role: user.role,
-    likeRecipes: user.likeRecipes,
   };
 
   const token = jwt.sign(payload, process.env.JWT_SECRET);
